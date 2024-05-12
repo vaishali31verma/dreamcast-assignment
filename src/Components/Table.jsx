2a88b1ef78d5c97e04d8eb6cfad2d63689e8c872
vaishali31verma <gadaiyavaishali31@gmail.com>
2024-05-12 21:56:50 +0530

    btn fixes

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -60,7 +60,7 @@ const Table = () => {
           <button
             onClick={handleFormOpen}
             type="button"
-            className="flex w-[200px] mt-3 mr self-end text-center h-11 mr-3 items-center bg-blue-950 justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+            className="flex w-[200px] mt-3 mr self-end text-center h-11 mr-3 items-center bg-blue-950 justify-center text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
             <svg
               className="h-[1.875rem] w-[1.875rem] mr-2 mt-2"
